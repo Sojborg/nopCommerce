18ecd91450b99febe692dbf81346ab1cada4708e
Sojborg <jsj@d60.dk>
2015-12-11 22:30:36 +0100

    first commit

---
+++ src/Plugins/Nop.Plugin.Payments.Dibs/Controllers/PaymentDibsController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Nop.Core;
+using Nop.Core.Domain.Payments;
+using Nop.Plugin.Payments.Dibs.Models;
+using Nop.Services.Configuration;
+using Nop.Services.Localization;
+using Nop.Services.Logging;
+using Nop.Services.Orders;
+using Nop.Services.Payments;
+using Nop.Services.Stores;
+using Nop.Web.Framework.Controllers;
+
+namespace Nop.Plugin.Payments.Dibs.Controllers
+{
+    public class PaymentDibsController : BasePaymentController
+    {
+        private readonly IWorkContext _workContext;
+        private readonly IStoreService _storeService;
+        private readonly ISettingService _settingService;
+        private readonly IPaymentService _paymentService;
+        private readonly IOrderService _orderService;
+        private readonly IOrderProcessingService _orderProcessingService;
+        private readonly ILogger _logger;
+        private readonly PaymentSettings _paymentSettings;
+        private readonly ILocalizationService _localizationService;
+
+        public PaymentDibsController()
+        {
+        }
+
+        [AdminAuthorize]
+        [ChildActionOnly]
+        public ActionResult Configure()
+        {
+
+            var model = new ConfigurationModel();
+            
+
+            return View("~/Plugins/Payments.Dibs/Views/PaymentDibs/Configure.cshtml", model);
+        }
+
+        [HttpPost]
+        [AdminAuthorize]
+        [ChildActionOnly]
+        public ActionResult Configure(ConfigurationModel model)
+        {
+            if (!ModelState.IsValid)
+                return Configure();
+            
+
+            //now clear settings cache
+            _settingService.ClearCache();
+
+            SuccessNotification(_localizationService.GetResource("Admin.Plugins.Saved"));
+
+            return Configure();
+        }
+
+        public override IList<string> ValidatePaymentForm(FormCollection form)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ProcessPaymentRequest GetPaymentInfo(FormCollection form)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Plugins/Nop.Plugin.Payments.Dibs/DibsPaymentProcesser.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Routing;
+using Nop.Core.Domain.Orders;
+using Nop.Core.Plugins;
+using Nop.Services.Payments;
+
+namespace Nop.Plugin.Payments.Dibs
+{
+    public class DibsPaymentProcesser : BasePlugin, IPaymentMethod
+    {
+        public ProcessPaymentResult ProcessPayment(ProcessPaymentRequest processPaymentRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PostProcessPayment(PostProcessPaymentRequest postProcessPaymentRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool HidePaymentMethod(IList<ShoppingCartItem> cart)
+        {
+            return false;
+        }
+
+        public decimal GetAdditionalHandlingFee(IList<ShoppingCartItem> cart)
+        {
+            return 0;
+        }
+
+        public CapturePaymentResult Capture(CapturePaymentRequest capturePaymentRequest)
+        {
+            var result = new CapturePaymentResult();
+            result.AddError("Capture method not supported");
+            return result;
+        }
+
+        public RefundPaymentResult Refund(RefundPaymentRequest refundPaymentRequest)
+        {
+            var result = new RefundPaymentResult();
+            result.AddError("Refund method not supported");
+            return result;
+        }
+
+        public VoidPaymentResult Void(VoidPaymentRequest voidPaymentRequest)
+        {
+            var result = new VoidPaymentResult();
+            result.AddError("Void method not supported");
+            return result;
+        }
+
+        public ProcessPaymentResult ProcessRecurringPayment(ProcessPaymentRequest processPaymentRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CancelRecurringPaymentResult CancelRecurringPayment(CancelRecurringPaymentRequest cancelPaymentRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CanRePostProcessPayment(Order order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Type GetControllerType()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether capture is supported
+        /// </summary>
+        public bool SupportCapture
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether partial refund is supported
+        /// </summary>
+        public bool SupportPartiallyRefund
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether refund is supported
+        /// </summary>
+        public bool SupportRefund
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether void is supported
+        /// </summary>
+        public bool SupportVoid
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Gets a recurring payment type of payment method
+        /// </summary>
+        public RecurringPaymentType RecurringPaymentType
+        {
+            get
+            {
+                return RecurringPaymentType.Manual;
+            }
+        }
+
+        /// <summary>
+        /// Gets a payment method type
+        /// </summary>
+        public PaymentMethodType PaymentMethodType
+        {
+            get
+            {
+                return PaymentMethodType.Standard;
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether we should display a payment information page for this plugin
+        /// </summary>
+        public bool SkipPaymentInfo
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public void GetPaymentInfoRoute(out string actionName, out string controllerName, out RouteValueDictionary routeValues)
+        {
+            actionName = "PaymentInfo";
+            controllerName = "PaymentDibs";
+            routeValues = new RouteValueDictionary { { "Namespaces", "Nop.Plugin.Payments.Dibs.Controllers" }, { "area", null } };
+        }
+
+        public void GetConfigurationRoute(out string actionName, out string controllerName, out RouteValueDictionary routeValues)
+        {
+            actionName = "Configure";
+            controllerName = "PaymentDibs";
+            routeValues = new RouteValueDictionary { { "Namespaces", "Nop.Plugin.Payments.Dibs.Controllers" }, { "area", null } };
+        }
+    }
+}
